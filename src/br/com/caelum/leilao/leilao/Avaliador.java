7f5803e28a882f7dbb9e8e632382ffcc72d920ab
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-08-29 10:03:06 -0300

    Initial commit

---
+++ src/br/com/caelum/leilao/leilao/Avaliador.java
@@ -0,0 +1,30 @@
+package br.com.caelum.leilao.leilao;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+
+public class Avaliador {
+
+	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+	private double menorDeTodos = Double.POSITIVE_INFINITY;
+
+	public void avaliar(Leilao leilao) {
+//		Optional<Lance> optionalMaximo = leilao.getLances().stream().max(Comparator.comparing(Lance::getValor));
+//		Optional<Lance> optionalMinimo = leilao.getLances().stream().min(Comparator.comparing(Lance::getValor));
+//		maiorDeTodos = optionalMaximo.orElseThrow(null).getValor();
+//		menorDeTodos = optionalMinimo.orElseThrow(null).getValor();
+		for (Lance lance : leilao.getLances())
+			if (lance.getValor() > this.maiorDeTodos)
+				this.maiorDeTodos = lance.getValor();
+			else if (lance.getValor() < this.menorDeTodos)
+				this.menorDeTodos = lance.getValor();
+	}
+
+	public double getMaiorDeTodos() {
+		return maiorDeTodos;
+	}
+
+	public double getMenorDeTodos() {
+		return menorDeTodos;
+	}
+}
---
+++ src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -0,0 +1,26 @@
+package br.com.caelum.leilao.teste;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import br.com.caelum.leilao.leilao.Avaliador;
+
+public class TesteDoAvaliador {
+
+	public static void main(String[] args) {
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("José");
+		Usuario maria = new Usuario("Maria");
+
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+		leilao.propoe(new Lance(joao, 250.0));
+		leilao.propoe(new Lance(jose, 300.0));
+		leilao.propoe(new Lance(maria, 400.0));
+
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avaliar(leilao);
+
+		System.out.println("Menor: " + leiloeiro.getMenorDeTodos());
+		System.out.println("Maior: " + leiloeiro.getMaiorDeTodos());
+	}
+}
