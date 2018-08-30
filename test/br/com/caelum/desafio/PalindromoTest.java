6cb8a0ddb791a28ec092617c21b59bb0c0bb8856
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-08-30 10:00:06 -0300

    Clean code test(2)

--- test/br/com/caelum/desafio/PalindromoTest.java
+++ test/br/com/caelum/desafio/PalindromoTest.java
@@ -7,13 +7,8 @@ public class PalindromoTest {
 
 	@Test
 	public void deveValidarPalindromos() {
-		// parte 1:cenario
 		Palindromo marrocos = new Palindromo();
-
-		// parte 2: acao
 		boolean resultado = marrocos.ehPalindromo("A base do teto desaba");
-
-		// parte 3: validacao
 		Assert.assertEquals(true, resultado);
 	}
 
